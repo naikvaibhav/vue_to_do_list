a72707a7f0501008bf9b6f6654f453af28453054
Sweta <sweta@umangsoftware.com>
2019-12-23 18:18:38 +0530

    new commit

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,38 @@
+<template>
+  <header class="header">
+    <h1>TodoList</h1>
+    <a v-bind:href="website">The world</a>
+    <div id="nav">
+      <router-link to="/">Home</router-link>|
+      <router-link to="/about">About</router-link>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  data() {
+    return {
+      website: "http://www.google.com"
+    };
+  }
+};
+</script>
+
+<style scoped>
+.header {
+  background: #333;
+  color: #fff;
+  text-align: center;
+  padding: 10px;
+}
+
+.header a {
+  color: #fff;
+  padding-right: 5px;
+  text-decoration: none;
+}
+</style>
+
+
